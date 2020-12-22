926400e5d28d873584908473a97150548ae4cabe
NilsMillot <compte.mxs@hotmail.fr>
2020-12-23 00:41:58 +0100

    up app.controller

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -9,4 +9,28 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/create-book')
+  createBook() {
+    return "deuxieme return";
+  }
+
+  @Get('/delete-book')
+  deleteBook() {
+    return "suppression d'un bouquin";
+  }
+  @Get('/read-book')
+  readBook() {
+    return "lecture d'un bouquin";
+  }
+
+  @Get('/reserve-book')
+  reserveBook() {
+    return "réservation d'un bouquin";
+  }
+  @Get('/report-rendering')
+  reportRenderingBook() {
+    return "signalement de rendu d'un livre";
+  }
+
 }
