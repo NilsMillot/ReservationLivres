da106d2f188f730ed38281dc176de51612b66c24
NilsMillot <compte.mxs@hotmail.fr>
2020-12-23 13:17:56 +0100

    up app.dto

--- back/src/app.dto.ts
+++ back/src/app.dto.ts
@@ -1,8 +1,24 @@
 export class BookDTO{
     title: string;
     id: number;
+    isReserved: boolean;
+    reservedById: number;
 }
 
 export class UserDTO{
-    id:number;
+    id: number;
+    isConnected: boolean;
+    role: ERole;
+}
+
+export class ReservationDTO{
+    id: number;
+    bookId: number;
+    startReserve: Date;
+    endReserve: Date;
+}
+
+enum ERole {
+    Admin = "ADMIN",
+    Reader = "READER",
 }
