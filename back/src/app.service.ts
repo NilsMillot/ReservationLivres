1028815a852c9ad2ac38a3255eafc46b74adb252
NilsMillot <compte.mxs@hotmail.fr>
2020-12-23 11:31:00 +0100

    up app.service and add book.module

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs';
+import { Book } from './book.module.ts';
+import { CreatePostDTO } from './dto/create-book.dto';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
+   constructor(@InjectModel('Book') private readonly postBook: Model<Book>) { }
+
+    getHello(): string {
+      return 'Hello World!';
+    }
+
+   async getBooks(): Promise<Book[]> {
+       const books = await this.postBook.find().exec();
+       return books;
+   }
+
+   async getBook(bookID: number): Promise<Book> {
+       const book = await this.postBook
+           .findById(bookID)
+           .exec();
+       return book;
+   }
+
+   async createBook(createBookDTO: CreateBookDTO): Promise<Book> {
+       const newBook = await this.postBook(createBookDTO);
+       return newBook.save();
+   }
+
+   async deleteBook(bookID: number): Promise<any> {
+      const deletedBook = await this.postBook
+          .findByIdAndRemove(bookID);
+      return deletedBook;
+    }
 }
