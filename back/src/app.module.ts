35569d0c279fc39b58e75f8ed19736cbc85a9d51
NilsMillot <compte.mxs@hotmail.fr>
2020-12-24 10:09:45 +0100

    up all

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { databaseProviders } from './database.providers';
 import { booksProviders } from './books.providers';
+import { databaseProviders } from './database.providers';
+// import { booksProviders } from './books.providers';
 
 @Module({
   imports: [], //import les modeles (user et book) (voir le dernier doc modeles envoyés par julien)
   controllers: [AppController],
-  providers: [AppService, ...databaseProviders, ...booksProviders],
-  exports: [...databaseProviders, ...booksProviders],
+  providers: [AppService,
+              ...databaseProviders,
+              ...booksProviders,
+  ],
+  exports: [...databaseProviders],
 })
 export class AppModule {}
